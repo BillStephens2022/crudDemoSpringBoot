969cd87b6724d61149f0974bdbf53125c9d78dc2
BillStephens2022 <two4onebill@yahoo.com>
2024-02-02 07:17:51 -0500

    successfully connected to mySQL database

--- src/main/java/com/billstephens/crudDemoSpringBoot/CrudDemoSpringBootApplication.java
+++ src/main/java/com/billstephens/crudDemoSpringBoot/CrudDemoSpringBootApplication.java
@@ -1,13 +1,22 @@
 package com.billstephens.crudDemoSpringBoot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudDemoSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudDemoSpringBootApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello World");
+		};
 	}
 
 }
