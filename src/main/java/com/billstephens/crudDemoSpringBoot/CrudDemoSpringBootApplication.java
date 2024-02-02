8d0b52ae153b47e03feb94f866db6e2058918103
BillStephens2022 <two4onebill@yahoo.com>
2024-02-02 07:49:49 -0500

    added a method to create a new Student and add it to the DB.  successfully tested

--- src/main/java/com/billstephens/crudDemoSpringBoot/CrudDemoSpringBootApplication.java
+++ src/main/java/com/billstephens/crudDemoSpringBoot/CrudDemoSpringBootApplication.java
@@ -1,5 +1,7 @@
 package com.billstephens.crudDemoSpringBoot;
 
+import com.billstephens.crudDemoSpringBoot.dao.StudentDAO;
+import com.billstephens.crudDemoSpringBoot.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +15,21 @@ public class CrudDemoSpringBootApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello World");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		// create the student object
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("Paul", "Doe", "paul@luv2code.com");
+		// save the student object
+		System.out.println("Saving the student ...");
+		studentDAO.save(tempStudent);
+		// display id of the saved student
+		System.out.println("Saved student.  Generated id: " + tempStudent.getId());
 	}
 
 }
