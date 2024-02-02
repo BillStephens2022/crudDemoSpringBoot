383de3556e13074dbedbecedc06b9d60fb3e4d48
BillStephens2022 <two4onebill@yahoo.com>
2024-02-02 07:43:34 -0500

    added interface and class for the Student DAO

---
+++ src/main/java/com/billstephens/crudDemoSpringBoot/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package com.billstephens.crudDemoSpringBoot.dao;
+
+import com.billstephens.crudDemoSpringBoot.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student theStudent);
+}
