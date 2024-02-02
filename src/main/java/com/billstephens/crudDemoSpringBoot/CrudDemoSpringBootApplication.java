53c783e6e272161e131126a94d2e5becb99d8243
BillStephens2022 <two4onebill@yahoo.com>
2024-02-02 06:28:03 -0500

    initial commit

---
+++ src/main/java/com/billstephens/crudDemoSpringBoot/CrudDemoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.billstephens.crudDemoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDemoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDemoSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/billstephens/crudDemoSpringBoot/CrudDemoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billstephens.crudDemoSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDemoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
